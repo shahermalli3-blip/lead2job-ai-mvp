e5fea2281188c8e5c5ed986ffbcf113b68158111
shahermalli3-blip <shahermalli3@gmail.com>
2026-08-28 18:26:33 +0200

    Retest website intake location guard

--- api/website-intake-smoke.js
+++ api/website-intake-smoke.js
@@ -1,7 +1,7 @@
 export default async function handler(req,res){
   const r=await fetch('https://npqiispbwwinnnvzyybx.supabase.co/functions/v1/receive-customer-message',{
     method:'POST',headers:{'Content-Type':'application/json'},
-    body:JSON.stringify({token:'a1d29e9b-ebf4-4145-b24e-614b96c81e8e',channel:'website',external_contact:'+31600000002',message:'Hoi, ik zoek schoonmaak.'})
+    body:JSON.stringify({token:'a1d29e9b-ebf4-4145-b24e-614b96c81e8e',channel:'website',external_contact:'+31600000003',message:'Hoi, ik zoek schoonmaak.'})
   });
   const data=await r.json();
   res.status(200).json({upstream_status:r.status,data});
